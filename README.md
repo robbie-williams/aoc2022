d583ae45440378a2fbf47dbbc1e259f28dc91fa2
Robert Williams <robert.williams@nib.com.au>
2022-12-07 01:40:24 +1100

    beginning day 3

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # aoc2022
 Just another coder having a crack at https://adventofcode.com/2022 challenges
 
-| Day         | Status      | Part 1      | Part 2      |
-| ----------- | ----------- | ----------- | ----------- |
-| 1           | Complete    | Done        | Done        |
-| 2           | Complete    | Done        | Done        |
+| Day | Status   | Part 1      | Part 2      |
+|-----|----------|-------------|-------------|
+| 1   | Complete | Done        | Done        |
+| 2   | Complete | Done        | Done        |
+| 3   | Started  | In-Progress | Not Started |
---
+++ src/main/kotlin/day3/Day3Main.kt
@@ -0,0 +1,36 @@
+package day3
+
+import day2.Game
+import java.io.File
+
+var rucksack: ArrayList<Array<Compartment>> = ArrayList()
+val itemPriorities: MutableMap<Char, Int> = HashMap()
+
+data class Compartment(val items: MutableMap<Char, CompartmentItem>)
+
+data class CompartmentItem(var count: Int)
+
+fun main() {
+    val bufferedReader = File("scratch_folder/day3-scratch-input.txt").bufferedReader()
+    val games: MutableList<Game> = ArrayList()
+    var gameIndex = 0
+    var roundIndex = 0
+    games.add(Game(gameIndex, ArrayList()))
+    bufferedReader.useLines { lines ->
+        lines.forEach { it ->
+            if (it.isEmpty()) {
+                val comparmentA = Compartment(HashMap())
+                    it.substring(0,Math.floorDiv(it.length,2)).forEach{char->
+                        comparmentA.items[char]?.count = comparmentA.items[char]?.count!! + 1
+                    }
+                val comparmentB = Compartment(HashMap())
+                it.substring(Math.floorDiv(it.length,2),it.length).forEach{char->
+                    comparmentB.items[char]?.count = comparmentB.items[char]?.count!! + 1
+                }
+
+                rucksack.add(arrayOf(comparmentA, comparmentB) )
+            }
+        }
+    }
+}
+
