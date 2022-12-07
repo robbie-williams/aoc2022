ac4f62d1776a7dd210bf45dd9a5371af4f570f10
Robert Williams <robert.williams@nib.com.au>
2022-12-07 21:23:07 +1100

    Day 3 part one complete

--- src/main/kotlin/day3/Day3Main.kt
+++ src/main/kotlin/day3/Day3Main.kt
@@ -2,7 +2,7 @@ package day3
 
 import java.io.File
 
-var rucksack: ArrayList<Array<Compartment>> = ArrayList()
+var rucksacks: ArrayList<Rucksack> = ArrayList()
 val itemPriorities: MutableMap<Char, Int> = buildPriorities()
 
 fun buildPriorities(): MutableMap<Char, Int> {
@@ -17,37 +17,59 @@ fun buildPriorities(): MutableMap<Char, Int> {
     {
         priorityMap[Char(i)]=i-96
     }
-    println(priorityMap)
     return priorityMap
 }
 
-data class Compartment(val items: MutableMap<Char, CompartmentItem>)
+data class Compartment(val label: String,
+                       val items: MutableMap<Char, CompartmentItem>)
+
+data class Rucksack(val compartments: Array<Compartment>,
+val duplicates: MutableList<Char>)
 
 data class CompartmentItem(var count: Int)
 
 fun main() {
-    val bufferedReader = File("scratch_folder/day3-scratch-input.txt").bufferedReader()
+    val bufferedReader = File("scratch_folder/day3-input.txt").bufferedReader()
 
     bufferedReader.useLines { lines ->
         lines.forEach { it ->
             if (it.isNotEmpty()) {
-                val compartmentA = Compartment(HashMap())
-                println("\n------------");
-                print("Compartment A: ")
-                it.substring(0,Math.floorDiv(it.length,2)).forEach{char->
-                    compartmentA.items[char]?.count = compartmentA.items[char]?.count!! + 1
-                    print("[$char]")
-                }
-                print(" || Compartment B: ")
-                val compartmentB = Compartment(HashMap())
-                it.substring(Math.floorDiv(it.length,2),it.length).forEach{char->
-                    compartmentB.items[char]?.count = compartmentB.items[char]?.count!! + 1
-                    print("[$char]")
+                rucksacks.add(Rucksack(arrayOf(
+                    Compartment("Compartment A", HashMap()), Compartment("Compartment B", HashMap())) ,ArrayList()) )
+                val rawCompartments = arrayOf(it.substring(0,Math.floorDiv(it.length,2)),
+                    it.substring(Math.floorDiv(it.length,2),it.length) )
+
+                for (i in 0..1)
+                {
+                    val compartment = rucksacks.last().compartments[i]
+                    rawCompartments[i].forEach{ char->
+                        if (compartment.items.containsKey(char))
+                        {
+                            compartment.items[char]?.count = compartment.items[char]?.count!! + 1
+                        }
+                        else
+                        {
+                            compartment.items[char] = CompartmentItem(1)
+                        }
+                        if (i == 1 &&
+                            rucksacks.last().compartments[0].items.containsKey(char) &&
+                            compartment.items[char]?.count!! == 1)
+                        {
+                            rucksacks.last().duplicates.add(char)
+                        }
+                    }
                 }
 
-                rucksack.add(arrayOf(compartmentA, compartmentB) )
+                println("Rucksack duplicates: ${rucksacks.last().duplicates}")
             }
         }
     }
+    var totalPriorities = 0
+    for (rucksack in rucksacks) {
+        rucksack.duplicates.forEach { char->
+            totalPriorities += itemPriorities[char]!!
+        }
+    }
+    println("Total Priorities: $totalPriorities")
 }
 
