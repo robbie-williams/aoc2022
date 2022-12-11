b9782a5ee4c86a9b6ec8bd60180d4cc877ad5974
Robert Williams <robert.williams@nib.com.au>
2022-12-11 18:14:38 +1100

    Day 5 part 1 complete

--- src/main/kotlin/day5/Day5Main.kt
+++ src/main/kotlin/day5/Day5Main.kt
@@ -10,11 +10,11 @@ data class LoadingBay( val crates: Stack<Crate>)
 
 data class Crate ( val crateLabel: String)
 
-data class Instruction( var instructionMap: Map<InstructionLoadingIndicator, Pair<Int, InstructionLoadingIndicator>> =
-                            mapOf( InstructionLoadingIndicator.AMOUNT to Pair(0, InstructionLoadingIndicator.FROM),
+data class Instruction(var instructionMap: MutableMap<InstructionLoadingIndicator, Pair<Int, InstructionLoadingIndicator>> =
+                            mutableMapOf( InstructionLoadingIndicator.AMOUNT to Pair(0, InstructionLoadingIndicator.FROM),
                                 InstructionLoadingIndicator.FROM to Pair(0, InstructionLoadingIndicator.TO),
                                 InstructionLoadingIndicator.TO to Pair(0, InstructionLoadingIndicator.AMOUNT)),
-                        var instructionLoadingIndicator: InstructionLoadingIndicator = InstructionLoadingIndicator.AMOUNT)
+                       var instructionLoadingIndicator: InstructionLoadingIndicator = InstructionLoadingIndicator.AMOUNT)
 
 
 enum class InstructionLoadingIndicator
@@ -30,7 +30,7 @@ enum class LineType{
     UNMATCHED
 }
 
-val myLineExpressions = mapOf<LineType,Pair<String, String>>(
+val myLineExpressions = mapOf(
 LineType.CRATE to Pair("^(([ \\[][ A-Z][ \\]]) {0,1})+?\$","( {0,1}   | {0,1}\\[[a-zA-Z]\\])"),
 LineType.BAY to Pair("^(?> [0-9]  {0,1})+\$","(\\d)+"),
 LineType.INSTRUCTION to Pair("^move ([0-9]+) from ([0-9]) to ([0-9])\$","([0-9]+)"),
@@ -38,10 +38,9 @@ LineType.UNMATCHED to Pair("","")
 )
 
 fun main() {
-    //Load up elves into a range-tree structure
     val bufferedReader = File("scratch_folder/day5-input.txt").bufferedReader()
     val dock = Dock(LinkedList(), ArrayList())
-    var currentInstruction = Instruction()
+    val currentInstruction = Instruction()
     bufferedReader.useLines { lines ->
         lines.forEachIndexed { lineNumber, line ->
             if (line.isNotEmpty()) {
@@ -49,23 +48,73 @@ fun main() {
                 LineType.values().forEach {
                     if (line.matches(Regex((myLineExpressions.get(it)!!.first)))) {
                         lineType = it
-                        print("\n------Line #$lineNumber: $line : $lineType: ")
                         myLineExpressions.get(it)!!.second.toRegex().findAll(line).forEachIndexed() { groupId, group ->
                             if (lineType == LineType.CRATE || lineType == LineType.BAY) {
                                 if (dock.loadingBays.size < groupId + 1)
                                     dock.loadingBays.add(LoadingBay(Stack()))
                                 if (lineType == LineType.CRATE && group.groupValues.last().trim().isNotEmpty())
-                                    dock.loadingBays[groupId].crates.push(Crate(group.groupValues.last()))
+                                    dock.loadingBays[groupId].crates.push(Crate(group.groupValues.last().trim()))
+                                if (lineType == LineType.BAY)
+                                    dock.loadingBays.forEach {loadingBay ->
+                                        loadingBay.crates.reverse()
+                                }
                             } else if (lineType == LineType.INSTRUCTION) {
-//                                currentInstruction.instructionMap[currentInstruction.instructionLoadingIndicator]!!.first = group.groupValues.last().trim().toInt()
+                                currentInstruction.instructionMap[currentInstruction.instructionLoadingIndicator] =
+                                    Pair(group.groupValues.last().trim().toInt(),
+                                        currentInstruction.instructionMap[currentInstruction.instructionLoadingIndicator]!!.second)
+                                currentInstruction.instructionLoadingIndicator = currentInstruction.instructionMap[currentInstruction.instructionLoadingIndicator]!!.second
                                 //Execute Instruction when TO is loaded
+                                if(currentInstruction.instructionLoadingIndicator == InstructionLoadingIndicator.AMOUNT) {
+                                    printDock(dock)
+                                    print(
+                                        "\nExecuting instruction to move " +
+                                                "${currentInstruction.instructionMap[InstructionLoadingIndicator.AMOUNT]!!.first} units from " +
+                                                "Bay ${currentInstruction.instructionMap[InstructionLoadingIndicator.FROM]!!.first } units to " +
+                                                "Bay ${currentInstruction.instructionMap[InstructionLoadingIndicator.TO]!!.first }")
+                                    for (i in 1..currentInstruction.instructionMap[InstructionLoadingIndicator.AMOUNT]!!.first) {
+                                        val crate = dock.loadingBays[currentInstruction.instructionMap[InstructionLoadingIndicator.FROM]!!.first-1].crates.pop()
+                                        print("\nMoving crate: ${crate.crateLabel}")
+                                        dock.loadingBays[currentInstruction.instructionMap[InstructionLoadingIndicator.TO]!!.first-1].crates.push(crate)
+                                    }
+                                }
                             }
-                            print("Group #$groupId: ${group.groupValues.last()} |")
                         }
                     }
                 }
             }
         }
     }
-    println("\nDock: $dock")
+    printDock(dock)
+    println("\nTop Crates: ")
+    dock.loadingBays.forEach { bay ->
+        print(bay.crates.peek().crateLabel)
+    }
+
+}
+
+fun printDock(dock: Dock)
+{
+    var maxHeight = 0
+    dock.loadingBays.forEachIndexed() { i, loadingBay ->
+        if (maxHeight < loadingBay.crates.size)
+        {
+            maxHeight = loadingBay.crates.size
+        }
+    }
+
+    println("\n-----Dock---")
+    for (currentHeight in maxHeight downTo 1 ) {
+        dock.loadingBays.forEach { loadingBay ->
+            if (currentHeight <= loadingBay.crates.size) {
+                print(loadingBay.crates[currentHeight-1].crateLabel)
+            } else {
+                print("...")
+            }
+            print(".")
+        }
+        println()
+    }
+    for (i in 1..dock.loadingBays.size)
+        print(".${i}..")
+    println("\n-----End of Dock---")
 }
